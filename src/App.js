c0296458be3a76c99bbf1eb67b1e26a535cdf3f6
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-05-02 16:27:36 -0700

    updated home title

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
         { title: 'Contact', path: '/contact'}
       ],
       home: {
-        title: "Web Developer in training",
+        title: "Full-Stack Web Developer-in-Training",
         subTitle: "Check out some things I've made",
       },
       about: {
