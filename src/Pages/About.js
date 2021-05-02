99634a44977a8b9595637d649a9d981a9a6889b6
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-05-02 14:17:48 -0700

    edited content in about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,35 @@
 import React from "react";
+import Hero from "../Components/Hero";
+import Content from "../Components/Content"
 
 function About(props) {
-  return(
-    <p>Learn about me</p>
+  return (
+    <div>
+      <Hero title={props.title} />
+
+      <Content>
+        <p>
+          Hello, my name is Alicia Vega. I'm currently attending the University of
+          Arizona's Coding Bootcamp. I'm passionate about learning about code
+          and want to continue enhancing my skills.
+        </p>
+        <p>
+          I hope to one day combine my design skills and front-end knowledge to
+          make cool-looking websites.
+        </p>
+
+        <p>
+          Connect with me{" "}
+          <a
+            href="https://github.com/aliciavega731"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            on Github.
+          </a>
+        </p>
+      </Content>
+    </div>
   );
 }
 
